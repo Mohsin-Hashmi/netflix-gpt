b2fc8dce5a7524f50c65aa8c8891d17dd2f8771f
Mohsin Hashmi <mnadeemhashmi2000@gmail.com>
2025-02-16 15:24:33 +0500

    Developing the login component

--- netflix-gpt-app/src/App.js
+++ netflix-gpt-app/src/App.js
@@ -1,12 +1,8 @@
 
 import './App.css';
-
+import Body from './components/Body';
 function App() {
-  return (
-    <div className="App">
-      <h1 className='text-center'>Hello i am developing new project</h1>
-    </div>
-  );
+  return <Body/>;
 }
 
 export default App;
---
+++ netflix-gpt-app/src/components/Login.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Header from "./Header";
+import { Link } from "react-router-dom";
+const Login = () => {
+  return (
+    <>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/f268d374-734d-474f-ad13-af5ba87ef9fc/web/PK-en-20250210-TRIFECTA-perspective_2ad65b09-fa88-4c24-969d-b8df29753007_small.jpg"
+          alt="bg netflix"
+        />
+      </div>
+      <div className="relative h-screen">
+        <form
+          action=""
+          className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2  bg-black/70 w-[450px] py-[48px] px-[68px]"
+        >
+          <h1 className="text-white text-3xl font-bold mb-[28px] ">Sign In</h1>
+          <input type="email" placeholder="Email Address" className=" w-full p-4 mb-6 bg-black/70 text-white" />
+          <input type="password" placeholder="Password" className="w-full p-4 mb-6 bg-black/70 text-white" />
+          <button className=" bg-[#E50914] w-full text-white p-4 rounded-md  text-xl ">Sign In</button>
+          <p className="text-center text-white my-[16px]"> OR</p>
+          <div className="flex justify-between hover:underline">
+            <div className="flex gap-x-[10px] items-center">
+                <input type="checkbox" className="text-white" />
+                <p className="text-white">Remember Me</p>
+            </div>
+            <Link className="text-white">Forgot password?</Link>
+          </div>
+          <p className="text-[#ffffffb3] mt-[16px] text-base">New to Netflix? <Link to='' className="text-white hover:underline">Sign up now.</Link></p>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Login;
